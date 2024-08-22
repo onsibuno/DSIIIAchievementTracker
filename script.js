f4819641a7a4615e7ffb68ff2e7ca76088db9f36
MathisBuissonAdrar <onsibuno@gmail.com>
2024-08-22 11:57:51 +0200

    modified css for narrow windows, created .json for achievements informations and script.js to generated the checkboxes and their boxes, deleted them in .html

---
+++ script.js
@@ -0,0 +1,60 @@
+let sectionPrincipale = document.querySelector('section');
+
+
+// variables "nom" dans la fonction permet d'aller chercher l'objet en question dans le .json
+async function recupChecklist(nom) {
+    const newDiv = document.createElement('div');
+    const newImg = document.createElement('img');
+    newDiv.appendChild(newImg);
+    const newForm = document.createElement('form');
+    newDiv.appendChild(newForm);
+
+    let test = await fetch('./checklist.json');
+    const dataTransformed = await test.json();
+    
+    sectionPrincipale.append(newDiv);
+    
+    
+    
+    let imageAchievement = dataTransformed[nom].image;
+    console.log(imageAchievement)
+    
+    let valueCheckbox = dataTransformed[nom].value;
+    let newInput = null;
+    let newLabel = document.createElement('label');
+    let newBreak = "<br>"
+    newLabel.innerText = "";
+
+    // Boucle qui créée les checkboxs pour chaque éléments du tableau
+    for (const nomCheckbox of dataTransformed[nom].checkboxes){
+
+        newLabel = document.createElement('label');
+        newInput = document.createElement('input');
+
+        // ajout des attributs aux checkbox et labels pour les liés
+        newInput.setAttribute("type", "checkbox");
+        newInput.setAttribute("name", nomCheckbox);
+        newInput.setAttribute("value", valueCheckbox);
+        newLabel.innerText = nomCheckbox;
+        newLabel.setAttribute("for", nomCheckbox);
+        console.log(nomCheckbox);
+
+        newForm.append(newInput);
+        newForm.innerHTML += " ";
+        newForm.append(newLabel);
+        newForm.innerHTML += newBreak;
+    }
+}
+
+
+//appel de la fonction pour chacun des objets
+
+recupChecklist("supremeWeaponReinforcement")
+recupChecklist("masterOfInfusions")
+recupChecklist("masterOfSorceries")
+recupChecklist("masterOfPyromancies")
+recupChecklist("masterOfMiracles")
+recupChecklist("masterOfRings")
+recupChecklist("masterOfExpressions")
+recupChecklist("ultimateBonfire")
+recupChecklist("ultimateEstus")
